3d9e55ab1f45985fcf4999f74c6ac730a6f20be9
William Parry <william.parry@techin.site>
2018-06-15 17:29:34 +1000

    Added UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,38 @@
 import React, { PureComponent } from 'react';
 import { cx } from 'emotion';
 import { Flattener } from 'solidifier/flattener';
-import { dropZone, dropZoneHovered } from 'solidifier/styles';
+import * as Styles from '../styles';
+
+import Logo from '../images/coinage_logo.svg';
 
 export default class IndexPage extends PureComponent {
 	state = {
 		hovered: false,
 		files: null,
+		selectedFile: null,
+		log: null,
 	};
-
-	fileContents = {};
 
 	constructor() {
 		super();
+		this.defaultState = this.state;
 		this.startParse = this.startParse.bind(this);
+		this.startAgain = this.startAgain.bind(this);
+		this.log = this.log.bind(this);
 	}
+
+	log = message => {
+		this.setState({
+			log: `${this.state.log || ''}\n${message}`.trim(),
+		});
+		setTimeout(() => {
+			this.textLog.scrollTop = this.textLog.scrollHeight;
+		}, 500);
+	};
 
 	addFile = (file, path) => {
 		const fullPath = path + file.name;
 		const files = Object.assign({}, this.state.files, { [fullPath]: file });
-
 		this.setState({ files });
 	};
 
@@ -77,46 +90,117 @@ export default class IndexPage extends PureComponent {
 		}
 	};
 
+	startAgain() {
+		this.setState(this.defaultState);
+	}
+
 	startParse = async function(path) {
 		// Create a new one each time so we have clean context
 		const result = await new Flattener().flatten(this.state.files, path);
 
-		this.setState({ result });
+		this.setState({ result, currentPath: path });
+
+		this.textResult.focus();
+		this.textResult.select();
+		this.textResult.scrollTop = 0;
 	};
 
 	render() {
-		const { hovered, files, result } = this.state;
-
-		if (result) {
-			return <pre>{result}</pre>;
-		}
+		const { hovered, files, result, log } = this.state;
 
 		return (
 			<div>
-				{!files && (
-					<div
-						className={cx(dropZone, { [dropZoneHovered]: hovered })}
-						onDragEnter={this.onDropZoneHover}
-						onDragOver={this.onDropZoneHover}
-						onDragLeave={this.onDropZoneLeave}
-						onDragEnd={this.onDropZoneLeave}
-						onDrop={this.onDrop}
-					>
-						<p>
-							{hovered ? 'Cool, now let go!' : 'Drag the folder containing your .sol files here...'}
-						</p>
-					</div>
-				)}
-
-				{files && (
-					<ul>
-						{Object.keys(files).map(path => (
-							<li key={path}>
-								<span onClick={() => this.startParse(path)}>{path}</span>
-							</li>
-						))}
+				<h1 className={Styles.header}>
+					<a href="https://coin.age">
+						<img src={Logo} alt="Coinage" />
+					</a>{' '}
+					Solidifier
+				</h1>
+				<div className={Styles.wrapper}>
+					<p className={Styles.introText}>
+						Takes solidity code and flattens it into a concatenated version ready for Etherscan's
+						verification tool.
+					</p>
+					<ul className={Styles.introList}>
+						<li>Runs 100% in the browser</li>
+						<li>
+							Use it to verify with <a href="https://etherscan.io">etherscan.io</a>
+						</li>
+						<li>
+							<a href="https://github.com/CoinageCrypto/solidifier">Open source</a>
+						</li>
 					</ul>
-				)}
+
+					{files && (
+						<div className={Styles.UIWrapper}>
+							<div className={Styles.console}>
+								<div className={Styles.consoleTop}>
+									<div className={Styles.fileList}>
+										<ul>
+											{Object.keys(files).map(path => (
+												<li key={path}>
+													<button
+														onClick={() => this.startParse(path)}
+														className={Styles.fileItem(path === this.state.currentPath)}
+													>
+														{path}
+													</button>
+												</li>
+											))}
+										</ul>
+									</div>
+
+									<div className={Styles.output}>
+										{!this.state.currentPath ? (
+											<p>← Select entry file to compile</p>
+										) : (
+											<textarea ref={textResult => (this.textResult = textResult)}>
+												{result}
+											</textarea>
+										)}
+									</div>
+								</div>
+
+								<div className={Styles.consoleBottom}>
+									<div className={Styles.bottomDrawer}>
+										<p>Log</p>
+										<textarea ref={textLog => (this.textLog = textLog)}>{log}</textarea>
+									</div>
+
+									<div className={Styles.actionsBar}>
+										<button onClick={this.startAgain}>↻ Start Again</button>
+									</div>
+								</div>
+							</div>
+							<div className={Styles.options}>
+								<h2>Options</h2>
+								<label>
+									<input type="checkbox" /> Insert file names
+								</label>
+								<label>
+									<input type="checkbox" /> Option 2
+								</label>
+							</div>
+						</div>
+					)}
+
+					{!files && (
+						<div
+							className={cx(Styles.dropZone, { [Styles.dropZoneHovered]: hovered })}
+							onDragEnter={this.onDropZoneHover}
+							onDragOver={this.onDropZoneHover}
+							onDragLeave={this.onDropZoneLeave}
+							onDragEnd={this.onDropZoneLeave}
+							onDrop={this.onDrop}
+						>
+							<p>
+								{hovered
+									? 'Cool, now let go!'
+									: 'Drag the folder containing your .sol files here...'}
+							</p>
+						</div>
+					)}
+				</div>
 			</div>
 		);
 	}
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,13 +1,195 @@
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 
 import './global.css';
 
+const bp = {
+	desktop: '@media (max-width: 991px)',
+	tablet: '@media (max-width: 767px)',
+	mobile: '@media (max-width: 479px)',
+};
+
 export const dropZone = css({
-	border: '5px solid blue',
-	width: '200px',
-	height: '100px',
+	border: '1px solid #333',
+	width: '100%',
+	height: '200px',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	boxShadow: '0 0 0 1px #fff inset',
+	'& p': {
+		margin: 0,
+	},
 });
 
 export const dropZoneHovered = css({
-	border: '5px solid red',
+	background: '#ccc',
+});
+
+export const introList = css({
+	'& li': {
+		color: '#333',
+		'& a': {
+			color: 'inherit',
+		},
+	},
+});
+
+export const UIWrapper = css({
+	display: 'flex',
+	flexDirection: 'row',
+});
+
+export const console = css({
+	border: '1px solid #333',
+	marginRight: '20px',
+	display: 'flex',
+	flexDirection: 'column',
+	flexGrow: 1,
+});
+
+export const consoleTop = css({
+	display: 'flex',
+});
+
+export const fileItem = selected =>
+	css({
+		display: 'block',
+		border: 'none',
+		padding: '10px 10px 10px 30px',
+		textAlign: 'right',
+		textDecoration: 'underline',
+		cursor: 'pointer',
+		background: 'transparent',
+		width: '100%',
+
+		...(selected
+			? {
+					background: '#333',
+					color: '#fff',
+			  }
+			: {
+					'&:hover': {
+						backgroundColor: '#ccc',
+					},
+			  }),
+	});
+
+export const options = css({
+	border: '1px solid #333',
+	width: '300px',
+
+	'& h2': {
+		margin: 0,
+		background: '#333',
+		color: '#fff',
+		fontWeight: 300,
+		fontSize: '14px',
+		padding: '10px',
+		marginBottom: '10px',
+	},
+	'& label': {
+		display: 'block',
+		padding: '10px',
+	},
+});
+
+export const wrapper = css({
+	padding: '20px',
+	maxWidth: '1400px',
+});
+
+export const header = css({
+	color: '#fff',
+	background: '#000',
+	fontWeight: 'normal',
+	textShadow: '0 2px 0 #b27200',
+	display: 'flex',
+	alignItems: 'center',
+	textTransform: 'uppercase',
+	letterSpacing: '1.5px',
+	lineHeight: 1,
+	fontWeight: 300,
+	margin: 0,
+	padding: '20px',
+	cursor: 'default',
+	'& img': {
+		verticalAlign: 'middle',
+		marginRight: '20px',
+	},
+});
+
+export const fileList = css({
+	borderRight: '1px solid #333',
+	flexShrink: 1,
+	'& ul': {
+		margin: 0,
+		padding: 0,
+		listStyle: 'none',
+		'& li': {
+			padding: 0,
+		},
+	},
+});
+
+export const output = css({
+	flexGrow: 1,
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	boxShadow: '0 0 0 1px #fff inset',
+	'& p': {
+		margin: 0,
+	},
+	'& textarea': {
+		border: 'none',
+		padding: '20px',
+		whiteSpace: 'pre',
+		width: '100%',
+		height: '100%',
+		background: '#fff',
+	},
+});
+
+export const introText = css({
+	margin: 0,
+	fontSize: '16px',
+});
+
+export const consoleBottom = css({
+	borderTop: '1px solid #333',
+	display: 'flex',
+	flexDirection: 'column',
+});
+
+export const bottomDrawer = css({
+	flexGrow: 1,
+	display: 'flex',
+	flexDirection: 'column',
+	margin: '10px',
+	'& p': {
+		margin: '0 0 5px 0',
+	},
+	'& textarea': {
+		border: '1px solid #333',
+		padding: '10px',
+		whiteSpace: 'pre',
+		width: '100%',
+		height: '100px',
+		background: '#fff',
+		outline: 'none',
+	},
+});
+
+export const actionsBar = css({
+	display: 'flex',
+	padding: '10px',
+	justifyContent: 'flex-end',
+	'& button': {
+		border: '1px solid #333',
+		background: '#ccc',
+		padding: '5px 15px',
+		'&:first-child': {
+			marginRight: '10px',
+		},
+	},
 });
