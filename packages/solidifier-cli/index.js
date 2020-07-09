bc037d96f27f1937719731e2501ac3267a450c55
Kevin Brown <kevin.brown@coina.ge>
2020-07-09 11:10:56 +1000

    Bumped and pinned some dependencies. Fixed some CLI bugs.

--- packages/solidifier-cli/index.js
+++ packages/solidifier-cli/index.js
@@ -22,75 +22,90 @@ const findSolFiles = (dir, relativePath = '', filelist = {}) => {
 	return filelist;
 };
 
-const argv = require('yargs')
-	// Examples
-	.example('')
-
-	// Help text
-	.alias('h', 'help')
-	.help('help')
-	.usage('Usage: $0 [options]')
-	.showHelpOnFail(true, 'Specify --help for available options')
-
-	// Input
-	.options({
-		c: {
-			alias: 'contract',
-			describe: 'The entry point contract to flatten.',
-			type: 'string',
-			normalize: true,
-			nargs: 1,
-		},
-		f: {
-			alias: 'insertFileNames',
-			describe: 'Includes the name of the files as a comment in the flattened output',
-			type: 'boolean',
-			default: false,
-		},
-		i: {
-			alias: 'inputDirectory',
-			describe:
-				'The directory where your contracts are located. Imports are resolved relative to this directory. Defaults to the directory the entry point contract is in.',
-			type: 'string',
-			normalize: true,
-			nargs: 1,
-		},
-		o: {
-			alias: 'outputDirectory',
-			describe: 'The directory for contract output. This will default outputSuffix to "" so the folder structure is maintained.',
-			type: 'string',
-			normalize: true,
-			nargs: 1,
-		},
-		s: {
-			alias: 'outputSuffix',
-			describe: 'The suffix to add to the output filename of the result. Defaults to `flattened`, e.g. <contract-name>-flattened.sol.',
-			type: 'string',
-			default: '-flattened',
-			nargs: 1,
-		},
-		w: {
-			alias: 'stripExcessWhitespace',
-			describe: 'Removes any excess whitespace from the output.',
-			type: 'boolean',
-			default: true,
-		}
-	})
-
-	// Version information
-	.alias('v', 'version')
-	.version(`Solidifier v${require('./package').version}`)
-	.describe('v', 'show version information')
-
-	// Copyright
-	.epilog(`Copyright ${new Date().getFullYear()} Coinage`)
-	
+const argv =
 	// Parse!
-	.argv;
+	require('yargs')
+		// Examples
+		.example('')
+
+		// Help text
+		.alias('h', 'help')
+		.help('help')
+		.usage('Usage: $0 [options]')
+		.showHelpOnFail(true, 'Specify --help for available options')
+
+		// Input
+		.options({
+			c: {
+				alias: 'contract',
+				describe: 'The entry point contract to flatten.',
+				type: 'string',
+				normalize: true,
+				nargs: 1,
+			},
+			f: {
+				alias: 'insertFileNames',
+				describe: 'Includes the name of the files as a comment in the flattened output',
+				type: 'boolean',
+				default: false,
+			},
+			i: {
+				alias: 'inputDirectory',
+				describe:
+					'The directory where your contracts are located. Imports are resolved relative to this directory. Defaults to the directory the entry point contract is in.',
+				type: 'string',
+				normalize: true,
+				nargs: 1,
+			},
+			o: {
+				alias: 'outputDirectory',
+				describe:
+					'The directory for contract output. This will default outputSuffix to "" so the folder structure is maintained.',
+				type: 'string',
+				normalize: true,
+				nargs: 1,
+			},
+			s: {
+				alias: 'outputSuffix',
+				describe:
+					'The suffix to add to the output filename of the result. Defaults to `flattened`, e.g. <contract-name>-flattened.sol.',
+				type: 'string',
+				default: '-flattened',
+				nargs: 1,
+			},
+			w: {
+				alias: 'stripExcessWhitespace',
+				describe: 'Removes any excess whitespace from the output.',
+				type: 'boolean',
+				default: true,
+			},
+		})
+
+		// Version information
+		.alias('v', 'version')
+		.version(`Solidifier v${require('./package').version}`)
+		.describe('v', 'show version information')
+
+		// Validate
+		.check((argv, options) => {
+			const { contract, inputDirectory } = argv;
+
+			if (!contract && !inputDirectory) {
+				throw new Error('You must pass either a contract or an input directory.');
+			}
+
+			return true;
+		})
+		// Copyright
+		.epilog(`Copyright ${new Date().getFullYear()} Coinage`).argv;
 
 // Let's prepare our options properly.
 // Some just come straight across
-const { outputDirectory, stripExcessWhitespace, insertFileNames } = argv;
+let { outputDirectory, stripExcessWhitespace, insertFileNames } = argv;
+let contracts = argv.contract ? [path.resolve(argv.contract)] : undefined;
+let inputDirectory = argv.inputDirectory
+	? path.resolve(argv.inputDirectory)
+	: path.dirname(argv.contract);
 
 // Others need a bit of massaging
 let { outputSuffix } = argv;
@@ -99,9 +114,6 @@ if (outputSuffix === '-flattened' && outputDirectory) {
 } else if (inputDirectory && !outputDirectory) {
 	outputDirectory = inputDirectory;
 }
-
-let contracts = argv.contract ? [path.resolve(argv.contract)] : undefined;
-let inputDirectory = argv.inputDirectory ? path.resolve(argv.inputDirectory) : path.dirname(contract);
 
 // Find all the contracts in the input directory
 const files = findSolFiles(inputDirectory);
@@ -121,22 +133,23 @@ for (let contract of contracts) {
 	const outputFile = path.join(directory, `${file}${outputSuffix}.sol`);
 
 	// Ok, time to process!
-	solidifier.flatten({
-		files,
-		path: contract,
-		insertFileNames,
-		stripExcessWhitespace,
-	}).then(result => {
-		console.log(`Saving ${outputFile}`)
-		const outputFullPath = path.join(outputDirectory, outputFile);
-		// Ensure the directory exists
-		mkdirp.sync(path.dirname(outputFullPath));
+	solidifier
+		.flatten({
+			files,
+			path: contract,
+			insertFileNames,
+			stripExcessWhitespace,
+		})
+		.then(result => {
+			console.log(`Saving ${outputFile}`);
+			const outputFullPath = path.join(outputDirectory, outputFile);
+			// Ensure the directory exists
+			mkdirp.sync(path.dirname(outputFullPath));
 
-		// Now save it
-		fs.writeFileSync(outputFullPath, result);
-
-	}).catch(error => {
-		
-		throw error
-	});
+			// Now save it
+			fs.writeFileSync(outputFullPath, result);
+		})
+		.catch(error => {
+			throw error;
+		});
 }
